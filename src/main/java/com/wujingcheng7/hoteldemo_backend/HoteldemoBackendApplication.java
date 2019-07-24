8dff0c873c2ab43afcd4d4558e790c14930f94b5
wujingcheng7 <690885595@qq.com>
2019-07-24 17:07:05 +0800

    Initial commit

---
+++ src/main/java/com/wujingcheng7/hoteldemo_backend/HoteldemoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wujingcheng7.hoteldemo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoteldemoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoteldemoBackendApplication.class, args);
+    }
+
+}
