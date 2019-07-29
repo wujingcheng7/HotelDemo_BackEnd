c6c8ad3d9a2d7d5b5d0103e8560715e42e2f5b8a
Xu Shown <xshawn301@gmail.com>
2019-07-29 16:25:48 +0800

    加入登录错误提示

--- src/main/java/com/wujingcheng7/hoteldemo_backend/controller/LoginController.java
+++ src/main/java/com/wujingcheng7/hoteldemo_backend/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/login")
@@ -20,12 +21,15 @@ public class LoginController {
     @GetMapping("")
     public String LoginHtml(){return "/login";}
     @PostMapping("")
-    public String login(@RequestParam("user_tel") String user_tel, @RequestParam("user_password") String user_password){
+    public String login(@RequestParam("user_tel") String user_tel, @RequestParam("user_password") String user_password,Model model){
         Result result;
         result = accountService.login(user_tel,user_password);
-        if(result.isSuccess())
-            return "redirect:/index";
-        else
-            return "用户名或密码错误";
+        if(result.isSuccess()) {
+            return "redirect:/index?{user_tem}";
+        }
+        else {
+            model.addAttribute("msg","用户名或密码错误");
+            return "/login";
+        }
     }
 }
--- src/main/java/com/wujingcheng7/hoteldemo_backend/service/HotelSearchService.java
+++ src/main/java/com/wujingcheng7/hoteldemo_backend/service/HotelSearchService.java
@@ -1,4 +1,16 @@
 package com.wujingcheng7.hoteldemo_backend.service;
 
+import com.wujingcheng7.hoteldemo_backend.mapper.HotelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/*
+* 酒店搜索
+* 1、城市、名称：HotelSimpleSearch(String hotel_city,Stirng hotel_name)
+* 2、城市、名称、日期:HotelDateSearch(String hotel_city,Stirng hotel_name,date indate,date outdate)
+* */
+@Service
 public class HotelSearchService {
+    @Autowired
+    private HotelMapper hotelMapper;
 }
