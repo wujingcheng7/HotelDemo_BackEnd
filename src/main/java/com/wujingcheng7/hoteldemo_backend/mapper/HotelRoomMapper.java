3955beef8e9d13da18dc94d75aebf37a805abc82
wujingcheng7 <690885595@qq.com>
2019-07-30 15:20:17 +0800

    对多个文件的名字进行了修改，增加了几个文件，稍微修改了一下前端页面的一些内容

---
+++ src/main/java/com/wujingcheng7/hoteldemo_backend/mapper/HotelRoomMapper.java
@@ -0,0 +1,16 @@
+package com.wujingcheng7.hoteldemo_backend.mapper;
+
+import com.wujingcheng7.hoteldemo_backend.domain.HotelRoom;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface HotelRoomMapper {
+    @Select("select * from hotel_room where hotel_id = #{hotel_id}")
+    List<HotelRoom> getHotelRoomListByHotelId(@Param("hotel_id")String hotel_id);
+}
--- src/main/java/com/wujingcheng7/hoteldemo_backend/service/HotelService.java
+++ src/main/java/com/wujingcheng7/hoteldemo_backend/service/HotelService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class HotelSearchService {
+public class HotelService {
     @Autowired
     HotelMapper hotelMapper;
 
