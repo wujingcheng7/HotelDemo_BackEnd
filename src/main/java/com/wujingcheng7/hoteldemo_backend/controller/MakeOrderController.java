6664989a3201fe5a8d91a076a2a7e9087824235f
douxuenong <xshawn301@gmail.com>
2019-07-31 13:47:02 +0800

    完成酒店预定功能

--- src/main/java/com/wujingcheng7/hoteldemo_backend/controller/MakeOrderController.java
+++ src/main/java/com/wujingcheng7/hoteldemo_backend/controller/MakeOrderController.java
@@ -1,29 +1,43 @@
 package com.wujingcheng7.hoteldemo_backend.controller;
 
-import com.wujingcheng7.hoteldemo_backend.domain.OrderList;
 import com.wujingcheng7.hoteldemo_backend.service.OrderlistService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
-@RequestMapping("/make_order")
+@RequestMapping("/make_an_order")
 public class MakeOrderController {
     @Autowired
     private OrderlistService orderlistService;
 
-    public String MakeOrderList(HttpServletRequest request){
+    @InitBinder
+    public void initBinder(WebDataBinder binder, WebRequest request) {
+
+        //转换日期
+        DateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd");
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));// CustomDateEditor为自定义日期编辑器
+    }
+
+    @PostMapping("")
+    public String MakeOrderList(HttpServletRequest request, @Param("hotel_room_id")String hotel_room_id,@Param("order_indate")Date order_indate,@Param("order_outdate")Date order_outdate){
         HttpSession session = request.getSession();
         String user_tel = (String)session.getAttribute("user_tel");
-        OrderList orderList = new OrderList();
-        orderList.setUser_tel(user_tel);
-        orderList.setHotel_id((String)request.getAttribute("hotel_id"));
-        orderList.setRoom_id((String)request.getAttribute("room_id"));
-        orderList.setHotel_room_id((String)request.getAttribute("hotel_room_id"));
-//        orderList.
+        String hotel_id = hotel_room_id.substring(0,6);
+        String room_id = hotel_room_id.substring(6);
+        orderlistService.createAnOrder(user_tel,hotel_id,room_id,order_indate,order_outdate);
+        return "/books_display";
     }
 }
