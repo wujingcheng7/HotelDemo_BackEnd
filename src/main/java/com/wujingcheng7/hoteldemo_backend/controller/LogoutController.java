42bf76e5d5d5311f1cdf584595f4535094c76b02
douxuenong <xshawn301@gmail.com>
2019-08-01 15:56:05 +0800

    加入退出登录功能

---
+++ src/main/java/com/wujingcheng7/hoteldemo_backend/controller/LogoutController.java
@@ -0,0 +1,20 @@
+package com.wujingcheng7.hoteldemo_backend.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/logout")
+public class LogoutController {
+    @GetMapping("")
+    public String Logout(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        session.invalidate();//清除session
+        return "/login";
+    }
+}
