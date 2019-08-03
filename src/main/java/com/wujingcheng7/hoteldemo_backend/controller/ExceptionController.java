99d38cc0ff914fce172e43a214df945385eb4e6e
wujingcheng7 <690885595@qq.com>
2019-08-03 16:39:30 +0800

    后端异常处理

---
+++ src/main/java/com/wujingcheng7/hoteldemo_backend/controller/ExceptionController.java
@@ -0,0 +1,22 @@
+package com.wujingcheng7.hoteldemo_backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/error")
+public class ExceptionController {
+    @GetMapping("/error")
+    public String getHtml(Model model, HttpServletRequest servletRequest,Exception e){
+        String wrong_url = servletRequest.getRequestURL().toString();
+        String message = e.getMessage();
+        model.addAttribute("wrong_url",wrong_url);
+        model.addAttribute("message",message);
+        return "/error";
+    }
+
+}
