8ad818f50b61ff24864a9798a3f2a27f1f948b4c
wujingcheng7 <690885595@qq.com>
2019-08-04 17:54:03 +0800

    后台修改酒店房间信息功能，后端，未完

---
+++ src/main/java/com/wujingcheng7/hoteldemo_backend/controller/back/HotelRoomsInfoBackEndController.java
@@ -0,0 +1,33 @@
+package com.wujingcheng7.hoteldemo_backend.controller.back;
+
+import com.wujingcheng7.hoteldemo_backend.domain.HotelRoom;
+import com.wujingcheng7.hoteldemo_backend.service.HotelRoomService;
+import com.wujingcheng7.hoteldemo_backend.service.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+@RequestMapping("hotelrooms_info_backend")
+public class HotelRoomsInfoBackEndController {
+    @Autowired
+    HotelService hotelService;
+    @Autowired
+    HotelRoomService hotelRoomService;
+
+    @GetMapping("")
+    public Model goHtml(HttpServletRequest request, Model model){
+        HttpSession session = request.getSession();
+        String hotel_id = (String) session.getAttribute("hotel_id");
+        List<HotelRoom> hotelRoomList = hotelRoomService.getRoomListByHotelId(hotel_id);
+        model.addAttribute("roomlist",hotelRoomList);
+        return model;
+    }
+
+}
