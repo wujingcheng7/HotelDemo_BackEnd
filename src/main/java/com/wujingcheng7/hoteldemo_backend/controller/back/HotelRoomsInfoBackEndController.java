5cc879cb0208debc8503ae8756b9078df9eee959
wujingcheng7 <690885595@qq.com>
2019-08-05 17:19:32 +0800

    在修改房间信息的页面内选择不同的房间号时，异步加载相关信息。

--- src/main/java/com/wujingcheng7/hoteldemo_backend/controller/back/HotelRoomsInfoBackEndController.java
+++ src/main/java/com/wujingcheng7/hoteldemo_backend/controller/back/HotelRoomsInfoBackEndController.java
@@ -6,8 +6,7 @@ import com.wujingcheng7.hoteldemo_backend.service.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -30,4 +29,11 @@ public class HotelRoomsInfoBackEndController {
         return model;
     }
 
+    @PostMapping("/{roomId}")
+    @ResponseBody
+    public HotelRoom getHotelRoom(@PathVariable("roomId") String roomId, HttpServletRequest request){
+        HttpSession session = request.getSession();
+        String hotel_id = (String)session.getAttribute("hotel_id");
+        return hotelRoomService.getRoomByHotelRoomId(hotel_id+roomId);
+    }
 }
