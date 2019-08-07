2e2d477703f989f6ef27a76edc1bfbdee98ac462
linyangxin0 <1227143051@qq.com>
2019-08-07 10:17:38 +0800

    Merge branch 'master' of D:\idea_project\HotelDemo_BackEnd with conflicts.

--- src/main/java/com/wujingcheng7/hoteldemo_backend/mapper/HotelRoomMapper.java
+++ src/main/java/com/wujingcheng7/hoteldemo_backend/mapper/HotelRoomMapper.java
@@ -4,6 +4,8 @@ import com.wujingcheng7.hoteldemo_backend.domain.HotelRoom;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,4 +18,15 @@ public interface HotelRoomMapper {
 
     @Select("select * from hotel_room where hotel_room_id = #{hotel_room_id}")
     HotelRoom getHotelRoomByHoteRoomlId(String hotel_room_id);
+
+    @Update("update hotel_room set room_type=#{room_type} where hotel_room_id=#{hotel_room_id}")
+    Boolean updateRoomTypeById(String hotel_room_id,String room_type);
+
+    @Update("update hotel_room set room_price=#{room_price} where hotel_room_id=#{hotel_room_id}")
+    Boolean updateRoomPriceById(String hotel_room_id,String room_price);
+
+    @Update("update hotel_room set room_text=#{room_text} where hotel_room_id=#{hotel_room_id}")
+    Boolean updateRoomTextById(String hotel_room_id,String room_text);
+
+
 }
